ab584ce42fdb48d099c97e13ab4d574fea177511
Jeremias Erbs <jerry@badabam.de>
2022-02-16 16:13:38 +0100

    Add simple routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import UserStatusPage from './pages/UserStatusPage.js';
+
 function App() {
   return (
-    <div>Hello App</div>
+    <div>
+      <nav>
+        <LinkButton to="/">User status</LinkButton>
+        <LinkButton to="/ships">Available ships</LinkButton>
+        <LinkButton to="/market">Marketplace</LinkButton>
+      </nav>
+      <Routes>
+        <Route path="/" element={<UserStatusPage />} />
+        <Route path="/ships" element={<div>Available ships</div>} />
+        <Route path="/market" element={<div>Marketplace</div>} />
+      </Routes>
+    </div>
   );
 }
+
+const LinkButton = styled(NavLink)`
+  padding: 4px 12px;
+  border: 1px solid #bbb;
+  border-radius: 4px;
+  background-color: #eee;
+  text-decoration: none;
+  color: #333;
+
+  &.active {
+    background-color: hotpink;
+  }
+`;
 
 export default App;
---
+++ src/pages/UserStatusPage.js
@@ -0,0 +1,8 @@
+export default function UserStatusPage() {
+  return (
+    <main>
+      <h1>User status</h1>
+      <p>Here comes the user status.</p>
+    </main>
+  );
+}
