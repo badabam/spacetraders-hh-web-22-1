b80f3860053d6fd48a97c69a93ad8af927c9fdf0
Jeremias Erbs <jerry@badabam.de>
2022-02-16 17:14:06 +0100

    Add logic to get and save token

--- src/App.js
+++ src/App.js
@@ -1,35 +1,87 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation.js';
+import ShipsPage from './pages/ShipsPage.js';
 import UserStatusPage from './pages/UserStatusPage.js';
+import MarketPage from './pages/MarketPage.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState(loadFromLocal('token'));
+  const [user, setUser] = useState(null);
+  const [isUsernameTaken, setIsUsernameTaken] = useState(false);
+
+  useEffect(() => {
+    saveToLocal('token', token);
+
+    if (token && !user) {
+      getUserInfo(token);
+    }
+  }, [user, token]);
+
   return (
     <div>
-      <nav>
-        <LinkButton to="/">User status</LinkButton>
-        <LinkButton to="/ships">Available ships</LinkButton>
-        <LinkButton to="/market">Marketplace</LinkButton>
-      </nav>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<UserStatusPage />} />
-        <Route path="/ships" element={<div>Available ships</div>} />
-        <Route path="/market" element={<div>Marketplace</div>} />
+        <Route
+          path="/"
+          element={
+            <UserStatusPage
+              onLogin={loginUser}
+              user={user}
+              isUsernameTaken={isUsernameTaken}
+            />
+          }
+        />
+        <Route path="/ships" element={<ShipsPage />} />
+        <Route path="/market" element={<MarketPage />} />
       </Routes>
     </div>
   );
-}
 
-const LinkButton = styled(NavLink)`
-  padding: 4px 12px;
-  border: 1px solid #bbb;
-  border-radius: 4px;
-  background-color: #eee;
-  text-decoration: none;
-  color: #333;
+  async function loginUser(username) {
+    setIsUsernameTaken(false);
 
-  &.active {
-    background-color: hotpink;
+    const response = await fetch(
+      `https://api.spacetraders.io/users/${username}/claim`,
+      {
+        method: 'POST',
+      }
+    ).catch(error => {
+      console.log('ERROR', error.message);
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setToken(data.token);
+      setUser(data.user);
+    } else {
+      setIsUsernameTaken(true);
+    }
   }
-`;
+
+  async function getUserInfo(token) {
+    try {
+      const response = await fetch(
+        'https://api.spacetraders.io/my/account?token=' + token
+      );
+      const data = await response.json();
+      setUser(data.user);
+    } catch (error) {
+      console.error('ERROR:', error);
+    }
+  }
+
+  function saveToLocal(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  function loadFromLocal(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 export default App;
